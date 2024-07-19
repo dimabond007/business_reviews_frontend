b964379ea3c405a58ee19f7b3e101ad2642abc26
Omerkugel12 <omerkugel12@gmail.com>
2024-07-19 19:45:56 +0300

    AuthContext

---
+++ src/pages/BusinessListPage.tsx
@@ -0,0 +1,14 @@
+import { useAuth } from "@/context/AuthContext";
+import { Outlet } from "react-router-dom";
+
+function BusinessListPage() {
+  const { loggedInUser } = useAuth();
+  return (
+    <>
+      <h1>BusinessListPage</h1>
+      <Outlet />
+    </>
+  );
+}
+
+export default BusinessListPage;
