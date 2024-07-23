51b1b86f80b9096015dfc50ae79bb549a9ec21a3
dimabond007 <dimistma@gmail.com>
2024-07-23 23:22:57 +0300

    Update API configuration to use environment variable

--- src/services/api.service.ts
+++ src/services/api.service.ts
@@ -2,13 +2,12 @@ import axios from "axios";
 
 const api = axios.create({
   baseURL:
-    process.env.NODE_ENV === "production" ? "/api" : "//localhost:3000/api",
+    process.env.NODE_ENV === "production" ? process.env.API_URL : "//localhost:3000/api",
 });
 
 api.interceptors.request.use(
   (config) => {
     let token = localStorage.getItem("jwt-taskify") as string;
-    // removing the first and last character of the token, which are quotes
     token = token?.slice(1, -1);
 
     if (token) config.headers.Authorization = `Bearer ${token}`;
